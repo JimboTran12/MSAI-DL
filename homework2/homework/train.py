f8bc850a8b1266731e8d6977f69c8af467e8a25b
Jimmy <hungtran2003ti@gmail.com>
2026-03-08 01:46:50 -0600

    update batchsixe to 512 and lr to 0.05  for mlp deep

--- homework2/homework/train.py
+++ homework2/homework/train.py
@@ -14,8 +14,8 @@ def train(
     exp_dir: str = "logs",
     model_name: str = "linear",
     num_epoch: int = 50,
-    lr: float = 1e-2,
-    batch_size: int = 32,
+    lr: float = 5e-2,
+    batch_size: int = 512,
     seed: int = 2024,
     **kwargs,
 ):
